b31aa2ce445274fc619425399ed03bdd8200b3b1
dedEbalay <lonshakov.max@gmail.com>
2021-04-27 15:07:19 +0300

    add

--- src/components/BeerCard/BeerCard.js
+++ src/components/BeerCard/BeerCard.js
@@ -4,7 +4,7 @@ const BeerCard = () => {
     return (
             <div className="w-25 p-3">
                 <div className="card" >
-                    <img src="..." class="card-img-top" alt="Пивко-лого" />
+                    <img src="..." className="card-img-top" alt="Пивко-лого" />
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
